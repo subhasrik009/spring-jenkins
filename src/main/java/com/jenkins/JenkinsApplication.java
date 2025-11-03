4ae31a3951bd473fa8405a2c9b7adc41544643fd
subhas.bagal <subhasrik009@gmail.com>
2025-11-04 00:35:17 +0530

    modified changes

--- src/main/java/com/jenkins/JenkinsApplication.java
+++ src/main/java/com/jenkins/JenkinsApplication.java
@@ -1,12 +1,24 @@
 package com.jenkins;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class JenkinsApplication {
 
+	public static Logger logger = LoggerFactory.getLogger(JenkinsApplication.class);
+	
+	@PostConstruct
+	public void init() {
+		logger.info("Application started");
+	}
+
 	public static void main(String[] args) {
+		logger.info("Application executed");
 		SpringApplication.run(JenkinsApplication.class, args);
 	}
 
